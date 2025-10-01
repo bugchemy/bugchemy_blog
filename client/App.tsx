3f7de48e78c639e2d490a0ec74f3446bbbfebdc8
Saurabh Katiyal <skatiyal@Saurabhs-MacBook-Pro.local>
2025-10-01 21:09:22 +0530

    removed header and footer from landing and comming soon page

--- client/App.tsx
+++ client/App.tsx
@@ -9,6 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useEffect } from "react";
 import { Content } from "@/lib/content";
 import Index from "./pages/Index";
+import ComingSoon from "./pages/ComingSoon";
 import Blog from "./pages/Blog";
 import Article from "./pages/Article";
 import About from "./pages/About";
@@ -43,6 +44,7 @@ const App = () => (
           <Route path="/contact" element={<Contact />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/auth" element={<Auth />} />
+          <Route path="/cs" element={<ComingSoon />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
